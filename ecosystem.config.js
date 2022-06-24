271d57eb3212d021b4a44083e12bfd57eccaba02
Anthony Salemo <ajssalemo@gmail.com>
2022-06-24 16:13:32 -0400

    testing with a different command - fixing

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,7 +2,8 @@ module.exports = {
   apps: [
     {
       name: "azure-webapps-linux-node-nextjs-javascript",
-      script: "./node_modules/next/dist/bin/next start",
+      script: "./node_modules/next/dist/bin/next",
+      args: "start",
       watch: false,
       env: {
         NODE_ENV: "production",
