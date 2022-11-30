3180e162d4b43502a7d5c7a6a905a5401ba2e8f4
Anthony Salemo <ajssalemo@gmail.com>
2022-11-30 17:10:02 -0500

    testing symlinks

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,7 +1,5 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
-export default function handler(req, res) {
-  console.log(process.env.NEXT_PUBLIC_API_URI);
-  console.log(process.env.API_URI);
+export default function handler(_req, res) {
   res.status(200).json({ name: "John Doe" });
 }
